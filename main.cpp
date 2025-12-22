80b4fe89ab54003ae67b375b7a1b37c1eb17dc23
polaariiis <forrlearninngg@gmail.com>
2025-12-22 15:43:49 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+	//intialize
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	
+	
+	sf::RenderWindow wd(sf::VideoMode(800, 600), "smth" , sf::Style::Default , settings);
+	sf::CircleShape shape(50.f);
+	shape.setFillColor(sf::Color::Red);
+	
+
+	//main game loop
+	while (wd.isOpen())
+	{
+		//update
+	sf::Event event;
+	while (wd.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed) {
+			wd.close();
+		}
+		}
+
+
+	//draw()
+	wd.clear(sf::Color::Black);
+	wd.draw(shape);
+	wd.display();
+	
+	}
+	
+}
